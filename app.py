3ef39d7b1905701eb2a5213ec2b0aee1fcc2ef7f
Dzeko <vivala0519@naver.com>
2021-08-03 05:02:09 +0900

    select 완성

--- app.py
+++ app.py
@@ -1,19 +1,75 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-
-import requests
-from bs4 import BeautifulSoup
-
+import json
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.project
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 ## API 역할을 하는 부분
+@app.route('/searchByYear', methods=['POST'])
+def searchByYear():
+    selectedYear = request.form['year']
+    data = db.kbo_batter_stat.distinct('team', {'year': int(selectedYear)})
+    print(data)
+    return jsonify(data)
+
+@app.route('/searchByTeam', methods=['POST'])
+def searchByTeam():
+    selectedYear = request.form['year']
+    selectedTeam = request.form['team']
+    hitter_data = db.kbo_batter_stat.find({'team': selectedTeam, 'year': int(selectedYear)})
+    pitcher_data = db.kbo_pitcher_stat.find({'team': selectedTeam, 'year': int(selectedYear)})
+    player = []
+    for item in hitter_data:
+        player.append(item['name'])
+    for item in pitcher_data:
+        player.append(item['name'])
+    print(player)
+    return jsonify(player)
+
+@app.route('/searchByPosition', methods=['POST'])
+def searchByPosition():
+    selectedYear = request.form['year']
+    selectedTeam = request.form['team']
+    selectedPosition = request.form['position']
+    print(selectedYear, selectedTeam, selectedPosition)
+    print(selectedPosition)
+    player = []
+    if selectedPosition != '투수':
+        if selectedPosition == '포수':
+            selectedPosition = 'C'
+        if selectedPosition == '1루수':
+            selectedPosition = '1B'
+        if selectedPosition == '2루수':
+            selectedPosition = '2B'
+        if selectedPosition == '3루수':
+            selectedPosition = '3B'
+        if selectedPosition == '유격수':
+            selectedPosition = 'SS'
+        if selectedPosition == '좌익수':
+            selectedPosition = 'LF'
+        if selectedPosition == '중견수':
+            selectedPosition = 'CF'
+        if selectedPosition == '우익수':
+            selectedPosition = 'RF'
+        if selectedPosition == '지명타자':
+            selectedPosition = 'DH'
+        hitter_data = db.kbo_batter_stat.find({'team': selectedTeam, 'year': int(selectedYear), 'position': selectedPosition})
+        for item in hitter_data:
+            player.append(item['name'])
+    else:
+        pitcher_data = db.kbo_pitcher_stat.find({'team': selectedTeam, 'year': int(selectedYear)})
+        for item in pitcher_data:
+            player.append(item['name'])
+    print(player)
+    return jsonify(player)
+
 @app.route('/review', methods=['POST'])
 def write_review():
     sample_receive = request.form['sample_give']
@@ -25,7 +81,8 @@ def write_review():
 def read_reviews():
     sample_receive = request.args.get('sample_give')
     print(sample_receive)
-    return jsonify({'msg': '이 요청은 GET!'})
+    return jsonify({'msg': ''})
+
 
 
 if __name__ == '__main__':
---
+++ static/page.js
@@ -0,0 +1,72 @@
+// 연도 선택 시
+$(document).on('change', '#yearSelect', function () {
+    let selectedYear = $(this).find('option:selected').text();
+    console.log(selectedYear);
+    $.ajax({
+        url:'/searchByYear',
+        type:'POST',
+        data : {'year' : selectedYear},
+        success: function(data) {
+            $('#team').empty();
+            $('#team').append('<option>팀 선택</option>')
+            console.log(data)
+            $.each(data, function(index, item){
+                let output = '<option>';
+                output += item;
+                output += '</option>';
+                $('#team').append(output);
+            });
+        }
+    })
+});
+
+// 팀 선택 시
+$(document).on('change', '#team', function() {
+    let selectedTeam = $(this).find('option:selected').text();
+    let selectedYear = $('#yearSelect').find('option:selected').text();
+    console.log(selectedTeam, selectedYear);
+    $.ajax({
+        url:'/searchByTeam',
+        type:'POST',
+        data : {'team' : selectedTeam, 'year': selectedYear},
+        success: function(data) {
+            $('#player').empty();
+            $('#position').empty();
+            let position_set = '<option>포지션 선택</option><option>포수</option> <option>1루수</option> <option>2루수</option>';
+            position_set += '<option>3루수</option> <option>유격수</option> <option>좌익수</option>';
+            position_set += '<option>우익수</option> <option>중견수</option> <option>지명타자</option> <option>투수</option>';
+            $('#position').append(position_set);
+            $('#player').append('<option>선수 선택</option>');
+            console.log(data)
+            $.each(data, function(index, item){
+                   let output = '<option>';
+                   output += item;
+                   output += '</option>';
+                   $('#player').append(output);
+                });
+        }
+    })
+});
+
+// 포지션 선택 시
+$(document).on('change', '#position', function() {
+    let selectedPosition = $(this).find('option:selected').text();
+    let selectedYear = $('#yearSelect').find('option:selected').text();
+    let selectedTeam = $('#team').find('option:selected').text();
+    console.log(selectedTeam, selectedYear, selectedPosition);
+    $.ajax({
+        url:'/searchByPosition',
+        type:'POST',
+        data : {'team' : selectedTeam, 'year': selectedYear, 'position': selectedPosition},
+        success: function(data) {
+            $('#player').empty();
+            console.log(data)
+            $.each(data, function(index, item){
+                   let output = '<option>';
+                   output += item;
+                   output += '</option>';
+                   $('#player').append(output);
+                });
+        }
+    })
+});
